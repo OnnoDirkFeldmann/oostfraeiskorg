f5b26a0fd60e56d81b6f609c6a1b0e71894ba784
Onno Feldmann <Neronno@NERONNO-XEON-X5>
2023-07-17 16:04:28 +0200

    Adminpage

---
+++ oostfraeiskorg/SQLCON.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.Data.Sqlite;
+
+namespace WFDOT
+{
+    public class SQLCON
+    {
+        public static SqliteConnection GetConnection(string startpath)
+        {
+            SqliteConnection connection = new SqliteConnection();
+            string dataSource = startpath + @"WFDOT.db";
+            connection.ConnectionString = $"Data Source={dataSource}; Pooling=False;";
+            connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ oostfraeiskorg/ViewModels/AdminViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using WFDOT;
+using Microsoft.Data.Sqlite;
+using DotVVM.Framework.ViewModel;
+
+namespace oostfraeiskorg.ViewModels
+{
+    public class AdminViewModel : MasterPageViewModel
+    {
+        [Bind(Direction.Both)]
+        public string Message { get; set; }
+
+        public void RecreateSitemap()
+        {
+            RecreateSitemapTask();
+            Message = "Sitemaps erstellt";
+        }
+
+        public void RecreateSitemapTask()
+        {
+            //Fixe Sitemap erstellen
+            string ls_sitemap_fixed;
+            ls_sitemap_fixed = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+            ls_sitemap_fixed += "<urlset xmlns=\"https://www.sitemaps.org/schemas/sitemap/0.9\">";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/</loc><changefreq>always</changefreq><priority>0.8</priority></url>";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/main.aspx</loc><changefreq>always</changefreq></url>";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/grammar.aspx</loc><changefreq>always</changefreq></url>";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/leerbauk.aspx</loc><changefreq>always</changefreq></url>";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/onlinecourse.aspx</loc><changefreq>always</changefreq></url>";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/keyboard.aspx</loc><changefreq>always</changefreq></url>";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/downloads.aspx</loc><changefreq>always</changefreq></url>";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/about.aspx</loc><changefreq>always</changefreq></url>";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/dsgvo.aspx</loc><changefreq>always</changefreq></url>";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/links.aspx</loc><changefreq>always</changefreq></url>";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/instructions.aspx</loc><changefreq>always</changefreq></url>";
+            ls_sitemap_fixed += "<url><loc>https://oostfraeisk.org/donateaword.aspx</loc><changefreq>always</changefreq></url>";
+            ls_sitemap_fixed += "</urlset>";
+            var sitemap_fixed = "sitemapfixed.xml";
+            using (StreamWriter writer = new StreamWriter(sitemap_fixed, false))
+            {
+                writer.WriteLine(ls_sitemap_fixed);
+            }
+
+            //Datenbankeiträge
+            var sqlCon = SQLCON.GetConnection("");
+            SqliteCommand sqlcmd = new SqliteCommand();
+            sqlcmd.Connection = sqlCon;
+            sqlcmd.CommandText = "SELECT Ostfriesisch FROM WB ORDER BY Ostfriesisch ASC";
+            SqliteDataReader reader = sqlcmd.ExecuteReader();
+            List<string> entrys = new List<string>();
+            while (reader.Read())
+            {
+                entrys.Add(reader.GetValue(0).ToString());
+            }
+            reader.Close();
+            sqlCon.Close();
+
+            string ls_sitemap_dynamic = "";
+            var index = 1;
+            var entryCount = 30000;
+            for (int i = 0; i < entrys.Count; i++)
+            {
+                if (i == 0 || i % entryCount == 1)
+                {
+                    ls_sitemap_dynamic = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+                    ls_sitemap_dynamic += "<urlset xmlns=\"https://www.sitemaps.org/schemas/sitemap/0.9\">";
+                }
+                ls_sitemap_dynamic += "<url><loc>https://oostfraeisk.org/main.aspx?W=" + entrys[i].Replace("'", "&apos;").Replace("\"", "&quot;").Replace(">", "&gt;").Replace("<", "&lt;") + "&amp;df=frs>de&amp;fts=J</loc><changefreq>always</changefreq></url>";
+                if ((i == entrys.Count - 1 || i % entryCount == 0) && i != 0)
+                {
+                    ls_sitemap_dynamic += "</urlset>";
+                    string sitemapxml_dynamic = "sitemapdynamic{index}.xml";
+                    using (StreamWriter writer = new StreamWriter(sitemapxml_dynamic, false))
+                    {
+                        writer.WriteLine(ls_sitemap_dynamic);
+                    }
+                    ls_sitemap_dynamic = "";
+                    index++;
+                }
+            }
+        }
+    }
+}
+
