1fbb8e326936f9102367078b8e349a00460de1d0
VanModers <49339876+VanModers@users.noreply.github.com>
2023-07-27 00:38:55 +0200

    söyek tüskenstand (popup funksjenæært no näit)

---
+++ oostfraeiskorg/Searcher.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using System.Drawing;
+using System.IO;
+using DotVVM.Framework.Controls;
+using static oostfraeiskorg.ViewModels.MasterPageViewModel;
+using System.Collections;
+using System.Linq;
+using System.Data;
+
+namespace WFDOT
+{
+    public class Searcher
+    {
+        class Row
+        {
+            public long ID;
+            public string Ostfriesisch, Deutsch, Englisch, Artikel, Nebenformen, Standardform;
+        }
+
+        public static IQueryable<Entry> SearchAndFill(string searchstr, string suchrichtung, string volltextsuche)
+        {
+            List<Entry> Entries = new List<Entry>();
+       
+            string originalSearch;
+            string cellEastFrisian = "";
+            string cellHeaderTranslation = "";
+            string NotFound = "";
+            string NeedInput = "";
+            var language = Languages.German;
+            string dialectaltrans = "";
+
+            searchstr = searchstr.Trim();
+            originalSearch = searchstr;
+
+            switch (suchrichtung)
+            {
+                case "DE>FRS":
+                case "FRS>DE":
+                    cellEastFrisian = "Ostfriesisch";
+                    cellHeaderTranslation = "Deutsch";
+                    NotFound = $"Es sind keine Daten für die Suche '{originalSearch}' gefunden worden";
+                    NeedInput = "Sie müssen mindestens ein Wort eingeben";
+                    language = Languages.German;
+                    dialectaltrans = "dialektal";
+                    break;
+                case "EN>FRS":
+                case "FRS>EN":
+                    cellEastFrisian = "East Frisian";
+                    cellHeaderTranslation = "English";
+                    NotFound = $"No data found for '{originalSearch}'";
+                    NeedInput = "you have to enter at least one word";
+                    language = Languages.English;
+                    dialectaltrans = "dialectal";
+                    break;
+            }
+
+            if (originalSearch.Equals(string.Empty))
+            {
+                Entries.Add(new Entry("Jī mautent minst äin wōrd ingēven", NeedInput));
+                return Entries.AsQueryable();
+            }
+
+            var sqlCon = SQLCON.GetConnection("");
+            var sqlcmd = new SqliteCommand();
+            sqlcmd.Connection = sqlCon;
+            var searchstrParamUpper = new SqliteParameter()
+            {
+                ParameterName = "@searchstrupper",
+                Value = searchstr.ToUpper()
+            };
+            var searchstrParamLower = new SqliteParameter() {
+                ParameterName = "@searchstrlower",
+                Value = searchstr.ToLower()
+            };
+            var searchstrParam = new SqliteParameter()
+            {
+                ParameterName = "@searchstr",
+                Value = searchstr
+            };
+
+            switch (suchrichtung)
+            {
+                case "de>frs":
+                    SearchStrings.DeFrs(volltextsuche, ref searchstr, ref sqlcmd);
+                    break;
+                case "frs>de":
+                    SearchStrings.FrsDe(volltextsuche, ref searchstr, ref sqlcmd);
+                    break;
+                case "en>frs":
+                    SearchStrings.EnFrs(volltextsuche, ref searchstr, ref sqlcmd);
+                    break;
+                case "frs>en":
+                    SearchStrings.FrsEn(volltextsuche, ref searchstr, ref sqlcmd);
+                    break;
+            }
+
+            sqlcmd.Parameters.Add(searchstrParamUpper);
+            sqlcmd.Parameters.Add(searchstrParamLower);
+            sqlcmd.Parameters.Add(searchstrParam);
+            sqlcmd.Prepare();
+
+            var reader = sqlcmd.ExecuteReader();
+            List<Row> rows = new List<Row>();
+            while (reader.Read())
+            {
+                Row values = new Row();
+                values.ID = reader.GetInt64("ID");
+                values.Ostfriesisch = reader.GetValue("Ostfriesisch").ToString();
+                values.Deutsch = reader.GetValue("Deutsch").ToString();
+                values.Englisch = reader.GetValue("Englisch").ToString();
+                values.Artikel = reader.GetValue("Artikel").ToString();
+                values.Nebenformen = reader.GetValue("Nebenformen").ToString();
+                values.Standardform = reader.GetValue("Standardform").ToString();
+                rows.Add(values);
+            }
+            reader.Close();
+            sqlCon.Close();
+
+            var ids = new List<long>();
+            var eastFrisianStrings = new List<string>();
+            var eastFrisianWords = new List<string>();
+            var b = new List<string>();
+            foreach (var row in rows)
+            {
+                ids.Add(row.ID);
+                var eastFrisianString = row.Ostfriesisch;
+                eastFrisianString += !row.Artikel.Equals("-") ? $" ({row.Artikel})" : "";
+                eastFrisianString += !row.Nebenformen.Equals("-") ? $"<br/>[{dialectaltrans}: {row.Nebenformen}]" : "";
+                eastFrisianString += !row.Standardform.Equals("-") ? $"<br/>[{row.Standardform}]" : "";
+                eastFrisianStrings.Add(eastFrisianString);
+                switch (language)
+                {
+                    case Languages.German:
+                        b.Add(row.Deutsch);
+                        break;
+                    case Languages.English:
+                        b.Add(row.Englisch);
+                        break;
+                }
+                eastFrisianWords.Add(row.Ostfriesisch);
+            }
+
+            for (int i = 0; i < ids.Count; i++)
+            {
+                Entry entry = new Entry(eastFrisianStrings[i], b[i]);
+                /*resultCellFrisian.Text = eastFrisianStrings[i];
+                resultCellTranslation.Text = b[i];
+                if (ids[i].Equals("0"))
+                {
+                    resultCellFrisian.Font.Italic = true;
+                    resultCellFrisian.BackColor = ColorTranslator.FromHtml("#99CCFF");
+                    resultCellTranslation.Font.Italic = true;
+                    resultCellTranslation.BackColor = ColorTranslator.FromHtml("#99CCFF");
+                    resultCellInfo.Text = "";
+                    resultCellInfo.BackColor = ColorTranslator.FromHtml("#99CCFF");
+                }
+                else
+                {
+                    var bud = new DetailButton
+                    {
+                        CssClass = "btn btn_secondary bg-primary",
+                        ImageUrl = "/img/info.png"
+                    };
+                    bud.Click += new ImageClickEventHandler(page.showPopup);
+                    bud.wordid = ids[i];
+                    bud.ToolTip = eastFrisianWords[i];
+                    resultCellInfo.Controls.Add(bud);
+
+                    string mp3 = $"{page.Server.MapPath(" / ")}rec/{eastFrisianWords[i]}.mp3";
+                    if (File.Exists(mp3))
+                    {
+                        var bus = new CustomImageButton
+                        {
+                            CssClass = "btn btn_secondary bg-primary",
+                            ImageUrl = "/img/sound.png"
+                        };
+                        bus.Click += new ImageClickEventHandler(page.soundButton);
+                        bus.word = eastFrisianWords[i];
+                        bus.ToolTip = eastFrisianWords[i];
+                        resultCellInfo.Controls.Add(bus);
+                    }
+                }
+                resultRow.Cells.Add(resultCellFrisian);
+                resultRow.Cells.Add(resultCellTranslation);
+                resultRow.Cells.Add(resultCellInfo);*/
+                Entries.Add(entry);
+            }
+            if (eastFrisianStrings.Count == 0)
+            {
+                Entry entry = new Entry("D'r bünt ğīn dóóten föör d' söyek '" + originalSearch + "' funnen worden", NotFound);
+                Entries.Add(entry);
+            }
+
+            /*switch (suchrichtung)
+            {
+                case "de>frs":
+                case "frs>de":
+                    page.Master.Page.Title = $"Suche nach {originalSearch}({page.df}) - Ōstfräisk wōrdenbauk - Ostfriesisches Wörterbuch";
+                    page.Master.Page.MetaDescription = $"Übersetzung für {originalSearch}({page.df}) auf Ostfriesisch - Wörterbuch der ostfriesischen Sprache";
+                    break;
+                case "en>frs":
+                case "frs>en":
+                    page.Master.Page.Title = $"Searched for {originalSearch}({page.df}) - Ōstfräisk wōrdenbauk - East Frisian Dictionary";
+                    page.Master.Page.MetaDescription = $"Translation for {originalSearch}({page.df}) into East Frisian - Dictionary of the East Frisian Language";
+                    break;
+            }*/
+            return Entries.AsQueryable();
+        }
+    }
+}
--- oostfraeiskorg/ViewModels/MasterPageViewModel.cs
+++ oostfraeiskorg/ViewModels/MasterPageViewModel.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using DotVVM.Framework.Controls;
 using DotVVM.Framework.ViewModel;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using WFDOT;
 
 namespace oostfraeiskorg.ViewModels
 {
@@ -10,11 +15,11 @@ namespace oostfraeiskorg.ViewModels
         public List<string> Languages { get => new List<string>() { "DE>FRS", "FRS>DE", "EN>FRS", "FRS>EN" }; }
 
         private string _selectedLanguage = "DE>FRS";
-        
-        public string SelectedLanguage 
-        { 
-            get => _selectedLanguage; 
-            set => _selectedLanguage = value; 
+
+        public string SelectedLanguage
+        {
+            get => _selectedLanguage;
+            set => _selectedLanguage = value;
         }
 
         private string _lbl_keyboard = "collapse";
@@ -31,19 +36,56 @@ namespace oostfraeiskorg.ViewModels
             set => _txt_search = value;
         }
 
+        private static IQueryable<Entry> GetData()
+        {
+            return new[]
+            {
+                new Entry("fründskup", "Freundschaft"),
+                new Entry("mäinskup", "Gemeinschaft"),
+                new Entry("hôp", "Hoffnung"),
+                new Entry("lauğ", "Dorf")
+            }.AsQueryable();
+        }
+
+        public GridViewDataSet<Entry> Entries { get; set; }
+
+        public MasterPageViewModel() {
+            Entries = new GridViewDataSet<Entry>();
+        }
+
+        public class Entry
+        {
+            public string Frisian { get; set; }
+            public string Translation { get; set; }
+
+            public Entry()
+            {
+                // NOTE: This default constructor is required. 
+                // Remember that the viewmodel is JSON-serialized
+                // which requires all objects to have a public 
+                // parameterless constructor
+            }
+
+            public Entry(string frisian, string translation)
+            {
+                Frisian = frisian;
+                Translation = translation;
+            }
+        }
+
         public void Search()
         {
-
+            Entries.LoadFromQueryable(WFDOT.Searcher.SearchAndFill(txt_search, SelectedLanguage, "J"));
         }
 
         public void SearchLike()
         {
-
+            Entries.LoadFromQueryable(WFDOT.Searcher.SearchAndFill(txt_search, SelectedLanguage, "N"));
         }
 
         public void SearchExact()
         {
-
+            Entries.LoadFromQueryable(WFDOT.Searcher.SearchAndFill(txt_search, SelectedLanguage, "X"));
         }
 
         public void btn_keyboard_Click()
