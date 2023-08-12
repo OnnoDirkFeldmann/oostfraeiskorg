e784ca22a71d613ca6ee104ae7db81e1ea6f7185
Onno Feldmann <Neronno@NERONNO-XEON-X5>
2023-08-12 13:39:30 +0200

    dóótenbīsğīrmen

---
+++ oostfraeiskorg/ViewModels/DsgvoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace oostfraeiskorg.ViewModels
+{
+    public class DsgvoViewModel : MasterPageViewModel
+    {
+        public override Task Init()
+        {
+            MasterPageTitle = "Datenschutzerklärung - Ōstfräisk Wōrdenbauk - Ostfriesisches Wörterbuch";
+            MasterPageDescription = "Datenschutz - Wörterbuch der ostfriesischen Sprache - Wörter aus dem Ostfriesischen oder ins Ostfriesische übersetzen. Die Sprache der Ostfriesen mit dem Wörterbuch für das ostfriesische Platt als Standardostfriesisch lernen.";
+            MasterPageKeywords = "Datenschutzerklärung, dsgvo, jungfräiske mäinskup, jfm, ostfriesische Sprache, ostfriesisch, ōstfräisk";
+            return base.Init();
+        }
+    }
+}
+
