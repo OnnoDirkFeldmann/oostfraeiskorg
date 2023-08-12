be46b2f53610ffa9c9164444b4268fd1288bbda6
Onno Feldmann <Neronno@NERONNO-XEON-X5>
2023-08-12 19:15:44 +0200

    onlainkurs blank

---
+++ oostfraeiskorg/ViewModels/lessons/Lesson2ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.Hosting;
+
+namespace oostfraeiskorg.ViewModels.lessons
+{
+    public class Lesson2ViewModel : oostfraeiskorg.ViewModels.MasterPageViewModel
+    {
+        
+    }
+}
+
