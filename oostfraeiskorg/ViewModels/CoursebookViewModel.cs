5b9632b5f82b8a497131aa8215bc72538d6ff645
Onno Feldmann <Neronno@NERONNO-XEON-X5>
2023-08-12 17:03:18 +0200

    tüskenstand

---
+++ oostfraeiskorg/ViewModels/CoursebookViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Metrics;
+using System.Threading.Tasks;
+
+namespace oostfraeiskorg.ViewModels
+{
+    public class CoursebookViewModel : MasterPageViewModel
+    {
+        public override Task Init()
+        {
+            MasterPageTitle = "Lehrbuch - Ōstfräisk Wōrdenbauk - Ostfriesisches Wörterbuch";
+            MasterPageDescription = "Lehrbuch - Wörterbuch der ostfriesischen Sprache - Wörter aus dem Ostfriesischen oder ins Ostfriesische übersetzen. Die Sprache der Ostfriesen mit dem Wörterbuch für das ostfriesische Platt als Standardostfriesisch lernen.";
+            MasterPageKeywords += "Lehrbuch, Kurs, lernen, jungfräiske mäinskup, jfm, ostfriesische Sprache, ostfriesisch, ōstfräisk";
+            return base.Init();
+        }
+    }
+}
+
