54683a676b7fb7b23cecdaa335eb336e4df1ed13
Onno Feldmann <Neronno@NERONNO-XEON-X5>
2023-08-12 20:14:17 +0200

    onlainkurs

--- oostfraeiskorg/ViewModels/lessons/Lesson2enViewModel.cs
+++ oostfraeiskorg/ViewModels/lessons/Lesson2enViewModel.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DotVVM.Framework.ViewModel;
-using DotVVM.Framework.Hosting;
+﻿using System.Threading.Tasks;
 
 namespace oostfraeiskorg.ViewModels.lessons
 {
     public class Lesson2enViewModel : oostfraeiskorg.ViewModels.MasterPageViewModel
     {
-        
+        public override Task Init()
+        {
+            MasterPageTitle = "lesson 2 - Ōstfräisk Wōrdenbauk - East Frisian Dictionary";
+            MasterPageDescription = "lesson 2 - Dictionary of the East Frisian language - translate words from East Frisian or into East Frisian. Learn the language of the East Frisians as Standard East Frisian with the Dictionary for East Frisian Platt.";
+            MasterPageKeywords += ", lesson 2, eastfrisian language, eastfrisian, ōstfräisk, course, course of eastfrisian language";
+            return base.Init();
+        }
     }
 }
 
--- oostfraeiskorg/ViewModels/lessons/Lesson3enViewModel.cs
+++ oostfraeiskorg/ViewModels/lessons/Lesson3enViewModel.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DotVVM.Framework.ViewModel;
-using DotVVM.Framework.Hosting;
+﻿using System.Threading.Tasks;
 
 namespace oostfraeiskorg.ViewModels.lessons
 {
     public class Lesson3enViewModel : oostfraeiskorg.ViewModels.MasterPageViewModel
     {
-        
+        public override Task Init()
+        {
+            MasterPageTitle = "lesson 3 - Ōstfräisk Wōrdenbauk - East Frisian Dictionary";
+            MasterPageDescription = "lesson 3 - Dictionary of the East Frisian language - translate words from East Frisian or into East Frisian. Learn the language of the East Frisians as Standard East Frisian with the Dictionary for East Frisian Platt.";
+            MasterPageKeywords += ", lesson 3, eastfrisian language, eastfrisian, ōstfräisk, course, course of eastfrisian language";
+            return base.Init();
+        }
     }
 }
 
