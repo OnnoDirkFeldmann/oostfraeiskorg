d123f2f533093e8ead0096615c9ced8419448846
Onno Feldmann <Neronno@NERONNO-XEON-X5>
2024-04-16 11:42:26 +0200

    tweitóólerğkaid

--- oostfraeiskorg/ViewModels/MasterPageViewModel.cs
+++ oostfraeiskorg/ViewModels/MasterPageViewModel.cs
@@ -12,20 +12,27 @@ public class MasterPageViewModel : DotvvmViewModelBase
 
     public List<string> Languages { get => new List<string>() { "DE>FRS", "FRS>DE", "EN>FRS", "FRS>EN" }; }
 
-    public string SelectedLanguage { get; set; } = "DE>FRS";
+    private string _selectedLanguage = "DE>FRS";
+    public string SelectedLanguage
+    {
+        get => _selectedLanguage;
+        set => _selectedLanguage = value;
+    }
 
+    public string InputPlaceHolder { get; set; } = "Gib ein deutsches Wort ein";
     public bool ShowKeyboard { get; set; } = false;
-
     public string SearchText { get; set; } = "";
     public string MasterPageTitle { get; set; } = "Oostfräisk Woordenbauk - Ostfriesisches Wörterbuch";
-
     public string MasterPageDescription { get; set; } = "Wörterbuch der ostfriesischen Sprache - Wörter aus dem Ostfriesischen oder ins Ostfriesische übersetzen. Die Sprache der Ostfriesen mit dem Wörterbuch für das ostfriesische Platt als Standardostfriesisch lernen.";
-
     public string MasterPageKeywords { get; set; } = "platt, ostfriesisch, Wörterbuch, Ostfriesland, plattdeutsch, ostfriesisich-deutsch Wörterbuch, deutsch-ostfriesisch-Wörterbuch, elektronisches Wörterbuch, Onlinewörterbuch, Wörterbuch online, ostfriesich online, oostfräisk, Jungfräiske Mäinskup, JFM, JFM Ostfriesland, Sprache von Ostfriesland, Ostfriesland Platt, Ostfriesisches Platt, ostfriesische Sprache, Regionalsprache, Wörterbuch der ostfriesischen Sprache, Weigelt, Schreibweise, Rechtschreibung, ostfriesische Schreibweise, friesisch schreiben, ostfriesisch lernen";
-
     public string AlertText { get; set; } = "";
-
     public bool AlertVisible { get; set; }
+    public string SearchButtonText { get; set; } = "Suche";
+    public string ExtendedSearchButtonText { get; set; } = "Erweiterte Suche";
+    public string SearchLikeButtonText { get; set; } = "Unscharfe Suche";
+    public string SearchBeginsButtonText { get; set; } = "Beginnt mit";
+    public string SearchEndsButtonText { get; set; } = "Endet mit";
+    public string SearchExactButtonText { get; set; } = "Exakte Suche";
 
     #region methods
 
@@ -52,6 +59,41 @@ public class MasterPageViewModel : DotvvmViewModelBase
     public void SearchEnds()
     {
         Context.RedirectToRoute("main", query: new { W = SearchText, df = SelectedLanguage.ToLower(), fts = "E" });
+    }
+
+    public void LanguageSelectionChanged()
+    {
+        switch (SelectedLanguage)
+        {
+            case "DE>FRS":
+                InputPlaceHolder = "Gib ein deutsches Wort ein";
+                SearchButtonText  = "Suche";
+                ExtendedSearchButtonText = "Erweiterte Suche";
+                SearchLikeButtonText = "Unscharfe Suche";
+                SearchBeginsButtonText = "Beginnt mit";
+                SearchEndsButtonText = "Endet mit";
+                SearchExactButtonText = "Exakte Suche";
+                break;
+            case "EN>FRS":
+                InputPlaceHolder = "enter a english word";
+                SearchButtonText = "search";
+                ExtendedSearchButtonText = "extended search";
+                SearchLikeButtonText = "fuzzy search";
+                SearchBeginsButtonText = "begins with";
+                SearchEndsButtonText = "ends with";
+                SearchExactButtonText = "exact search";
+                break;
+            case "FRS>DE":
+            case "FRS>EN":
+                InputPlaceHolder = "gif 'n oostfräisk woord in";
+                SearchButtonText = "söyken";
+                ExtendedSearchButtonText = "ferwiidert söyken";
+                SearchLikeButtonText = "unskaarp söyken";
+                SearchBeginsButtonText = "fangt an mit";
+                SearchEndsButtonText = "ent mit";
+                SearchExactButtonText = "eksakt söyken";
+                break;
+        }
     }
 
     #endregion
