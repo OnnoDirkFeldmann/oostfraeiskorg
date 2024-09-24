bfd4dfd6717ba2d7cab653009e824bcc35032067
Onno Feldmann <Neronno@NERONNO-XEON-X5>
2024-09-24 16:00:05 +0200

    groot skreeven

--- oostfraeiskorg/ViewModels/DownloadsViewModel.cs
+++ oostfraeiskorg/ViewModels/DownloadsViewModel.cs
@@ -7,7 +7,7 @@ public class DownloadsViewModel : MasterPageViewModel
     public override Task Init()
     {
         MasterPageTitle = "Downloads - Oostfräisk Woordenbauk - Ostfriesisches Wörterbuch";
-        MasterPageDescription = "Downloads - Wörterbuch der ostfriesischen Sprache - Wörter aus dem Ostfriesischen oder ins Ostfriesische übersetzen. Die Sprache der Ostfriesen mit dem Wörterbuch für das ostfriesische Platt als Standardostfriesisch lernen.";
+        MasterPageDescription = "Downloads - Wörterbuch der ostfriesischen Sprache - Wörter aus dem Ostfriesischen oder ins Ostfriesische übersetzen. Die Sprache der Ostfriesen mit dem Wörterbuch für das Ostfriesische Platt als Standardostfriesisch lernen.";
         MasterPageKeywords += ", downloads, jungfräiske mäinskup, jfm, ostfriesische Sprache, ostfriesisch, oostfräisk";
         return base.Init();
     }
--- oostfraeiskorg/ViewModels/lessons/Lesson4ViewModel.cs
+++ oostfraeiskorg/ViewModels/lessons/Lesson4ViewModel.cs
@@ -7,7 +7,7 @@ public class Lesson4ViewModel : oostfraeiskorg.ViewModels.MasterPageViewModel
     public override Task Init()
     {
         MasterPageTitle = "Lektion 4 - Oostfräisk Woordenbauk - Ostfriesisches Wörterbuch";
-        MasterPageDescription = "Lektion 4 - Wörterbuch der ostfriesischen Sprache - Wörter aus dem Ostfriesischen oder ins Ostfriesische übersetzen. Die Sprache der Ostfriesen mit dem Wörterbuch für das ostfriesische Platt als Standardostfriesisch lernen.";
+        MasterPageDescription = "Lektion 4 - Wörterbuch der ostfriesischen Sprache - Wörter aus dem Ostfriesischen oder ins Ostfriesische übersetzen. Die Sprache der Ostfriesen mit dem Wörterbuch für das Ostfriesische Platt als Standardostfriesisch lernen.";
         MasterPageKeywords += ", Lektion 4, ostfriesische Sprache, ostfriesisch, oostfräisk, Kurs, Kurs der ostfriesischen Sprache";
         return base.Init();
     }
